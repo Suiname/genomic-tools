4fa10168d368991e466479df9c72f38ff4fe9088
Jason Tham <jtham@sbcglobal.net>
2017-01-05 23:27:20 -0600

    questions up to 13 answered

--- week_1/Exam.sh
+++ week_1/Exam.sh
@@ -10,3 +10,19 @@ echo "How many genes have a single splice variant?"
 echo `cut -f1 apple.genes | uniq -c | more | grep " 1 " | wc -l`
 echo "How may genes have 2 or more splice variants?"
 echo `cut -f1 apple.genes | uniq -c | more | grep -v " 1 " | wc -l`
+echo "How many genes are there on the ‘+’ strand?"
+echo `cut -f1,4 apple.genes | grep "+" | uniq -c | wc -l`
+echo "How many genes are there on the ‘-’ strand?"
+echo `cut -f1,4 apple.genes | grep "-" | uniq -c | wc -l`
+echo "How many genes are there on chromosome chr1?"
+echo `cut -f1,3 apple.genes | grep "chr1" | uniq -c | wc -l`
+echo "How many genes are there on each chromosome chr2?"
+echo `cut -f1,3 apple.genes | grep "chr2" | uniq -c | wc -l`
+echo "How many genes are there on each chromosome chr3?"
+echo `cut -f1,3 apple.genes | grep "chr3" | uniq -c | wc -l`
+echo "How many transcripts are there on chr1?"
+echo `cut -f2,3 apple.genes | grep "chr1" | uniq -c | wc -l`
+echo "How many transcripts are there on chr2?"
+echo `cut -f2,3 apple.genes | grep "chr2" | uniq -c | wc -l`
+echo "How many transcripts are there on chr3?"
+echo `cut -f2,3 apple.genes | grep "chr3" | uniq -c | wc -l`
